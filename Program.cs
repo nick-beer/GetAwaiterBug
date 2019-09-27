66d0e6513d2ff4c990e97cdfd9bb9475ec1a3988
Nick Beer <nick.beer@ni.com>
2019-09-27 14:57:29 -0500

    Initial submission of repro

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Company.Name;
+
+namespace GetAwaiterBug
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("using System;");
+            await new ProgressToken();
+        }
+    }
+}
